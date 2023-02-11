8df56f30773461d933987cb1dc3cd6b61dbfb2cd
Umarsk77 <90238287+Umarsk77@users.noreply.github.com>
2023-02-11 14:50:43 +0530

    make home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StarRatingModule, StarRatingConfigService } from 'angular-star-rating';
+// import { StarRatingConfigService } from 'angular-star-rating/lib/services/star-rating-config.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StarRatingModule.forRoot()
+
+
   ],
-  providers: [],
+  providers: [
+    StarRatingConfigService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/model/food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+
+})
+export class HomeComponent implements OnInit {
+
+  foods:Food[]=[];
+
+  constructor (private api:FoodService){
+    this.foods = api.getAll()
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
